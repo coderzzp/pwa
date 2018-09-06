760b57d32755a7131bfa34c8165587646d43f2b6
斩玄 <zhanxuan@wacai.com>
2018-09-07 00:31:32 +0800

    add listener

--- src/workers.js
+++ src/workers.js
@@ -1,9 +1,10 @@
 export default function workers(){
+  console.log(window)
+  console.log(this)
   window.addEventListener('fetch', event => {
     alert(1)
       fetch(event.request.url).then(res=>{
         console.log(res)
       })
   })
-
 }
