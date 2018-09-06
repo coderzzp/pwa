b031e152e322d096a5347a7edcbfd74651f54468
斩玄 <zhanxuan@wacai.com>
2018-09-06 22:37:09 +0800

    add fetch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+    fetch('https://www.apiopen.top/satinApi?type=1&page=1')
+      .then(res=>{
+        res.json().then(({data})=>this.setState({data:data[0].text}))
+      })
+      
+  }
+  state = {
+    data:''
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +23,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.data}
       </div>
     );
   }
